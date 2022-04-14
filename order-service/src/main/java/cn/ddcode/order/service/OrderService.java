8da6a3056a5238cb68aaca460d0aa03235a3ba79
liuxiaoliang <15810140695@163.com>
2022-04-14 08:56:23 +0800

    HM_SpringCloud 初始化

---
+++ order-service/src/main/java/cn/ddcode/order/service/OrderService.java
@@ -0,0 +1,20 @@
+package cn.ddcode.order.service;
+
+import cn.ddcode.order.mapper.OrderMapper;
+import cn.ddcode.order.pojo.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        // 4.返回
+        return order;
+    }
+}
