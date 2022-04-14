afdb41dfc42a0b44566174adc9d312673ed5beb7
liuxiaoliang <15810140695@163.com>
2022-04-14 09:56:16 +0800

    HM_SpringCloud 完成restTemplate远程调用

--- order-service/src/main/java/cn/ddcode/order/service/OrderService.java
+++ order-service/src/main/java/cn/ddcode/order/service/OrderService.java
@@ -2,8 +2,12 @@ package cn.ddcode.order.service;
 
 import cn.ddcode.order.mapper.OrderMapper;
 import cn.ddcode.order.pojo.Order;
+import cn.ddcode.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @Service
 public class OrderService {
@@ -11,9 +15,17 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Resource
+    private RestTemplate restTemplate;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+
+        //使用restTemplate远程调用User模块
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
         // 4.返回
         return order;
     }
--- user-service/src/main/java/cn/ddcode/user/mapper/UserMapper.java
+++ user-service/src/main/java/cn/ddcode/user/mapper/UserMapper.java
@@ -3,7 +3,9 @@ package cn.ddcode.user.mapper;
 import cn.ddcode.user.pojo.User;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserMapper {
     
     @Select("select * from tb_user where id = #{id}")
